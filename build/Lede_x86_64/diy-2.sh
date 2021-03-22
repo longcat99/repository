285efbbf4dcba140cb8a00d78438f2c553912122
danshui-git <68673669+danshui-git@users.noreply.github.com>
2021-03-22 23:27:00 +0800

    Update diy-2.sh

--- build/Lede_x86_64/diy-2.sh
+++ build/Lede_x86_64/diy-2.sh
@@ -15,7 +15,7 @@ sed -i "/uci commit fstab/a\uci set network.lan.ipaddr='192.168.2.2'" $ZZZ
 sed -i "/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/argon" $ZZZ
 
 # 修改主机名称为OpenWrt-123
-sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'" $ZZZ
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'' $ZZZ
 
 # 版本号里显示一个自己的名字（把 ${Author} 改成你自己名字就行了，不改的话，默认使用你帐号）
 sed -i "s/OpenWrt /Compiled by ${Author} on $(TZ=UTC-8 date "+%Y/%m/%d") @ OpenWrt /g" $ZZZ
