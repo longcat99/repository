f87c23c0c67d06c2fad01fbf8e249889dbf2bffa
danshui-git <68673669+danshui-git@users.noreply.github.com>
2021-03-23 10:13:21 +0800

    Update diy-2.sh

--- build/Project_x86_64/diy-2.sh
+++ build/Project_x86_64/diy-2.sh
@@ -4,21 +4,19 @@ ZZZ="package/lean/default-settings/files/zzz-default-settings"
 #
 
 sed -i "/uci commit fstab/a\uci commit network" $ZZZ
-sed -i "/uci commit fstab/a\uci set network.lan.delegate='0'" $ZZZ
-sed -i "/uci commit fstab/a\uci set network.lan.dns='223.5.5.5 114.114.114.114'" $ZZZ
-sed -i "/uci commit fstab/a\uci set network.lan.broadcast='192.168.2.255'" $ZZZ
-sed -i "/uci commit fstab/a\uci set network.lan.gateway='192.168.2.1'" $ZZZ
-sed -i "/uci commit fstab/a\uci set network.lan.netmask='255.255.255.0'" $ZZZ
-sed -i "/uci commit fstab/a\uci set network.lan.ipaddr='192.168.2.2'" $ZZZ
+sed -i "/uci commit network/i\uci set network.lan.ipaddr='192.168.2.2'" $ZZZ
+sed -i "/uci commit network/i\uci set network.lan.netmask='255.255.255.0'" $ZZZ
+sed -i "/uci commit network/i\uci set network.lan.gateway='192.168.2.1'" $ZZZ
+sed -i "/uci commit network/i\uci set network.lan.broadcast='192.168.2.255'" $ZZZ
+sed -i "/uci commit network/i\uci set network.lan.dns='223.5.5.5 114.114.114.114'" $ZZZ
+sed -i "/uci commit network/i\uci set network.lan.delegate='0'" $ZZZ
 
 # 修改默认主题为argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i "/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/argon" $ZZZ
 
 # 修改主机名称为OpenWrt-123
 sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'" $ZZZ
-
-# 版本号里显示一个自己的名字（把 ${Author} 改成你自己名字就行了，不改的话，默认使用你帐号）
-sed -i "s/OpenWrt /Compiled by ${Author} on $(TZ=UTC-8 date "+%Y/%m/%d") @ OpenWrt /g" $ZZZ
 
 # 设置密码为空
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
